706a0c09f73df20b1726b6af2ab07c3fed110f8f
Anitha Murugan <mukanila101997@gmail.com>
2022-04-13 20:16:08 +0530

    Commited the build package

--- controllers/user.js
+++ controllers/user.js
@@ -28,13 +28,37 @@ exports.userLogin = (req, res, next) => {
     let fetchedUser;
     User.findOne({ email: req.body.email })
         .then(user => {
-            if (!user) {
+            if (!user && req.body.type == 'login') {
                 return res.status(401).json({
                     message: "Auth failed"
                 });
+            } else if (!user && req.body.type == 'socialLogin') {
+                bcrypt.hash(req.body.password, 10).then(hash => {
+                    const user = new User({
+                        email: req.body.email,
+                        password: hash
+                    });
+                    user.save()
+                        .then(result => {
+                            res.status(201).json({
+                                message: "User created!",
+                                result: result
+                            });
+                        })
+                        .catch(err => {
+                            res.status(500).json({
+                                message: "There's a problem to save user!"
+                            });
+                        });
+                });
             }
             fetchedUser = user;
-            return bcrypt.compare(req.body.password, user.password);
+            if (req.body.type == 'login') {
+                return bcrypt.compare(req.body.password, user.password);
+            } else if (req.body.type == 'socialLogin') {
+                return req.body.password;
+
+            }
         })
         .then(result => {
             if (!result) {
@@ -46,9 +70,11 @@ exports.userLogin = (req, res, next) => {
                 process.env.JWT_KEY, { expiresIn: "1h" }
             );
             res.status(200).json({
+                status: 'User Created',
                 token: token,
                 expiresIn: 3600,
-                userId: fetchedUser._id
+                userId: fetchedUser._id,
+                type: req.body.type
             });
         })
         .catch(err => {
--- routes/user.js
+++ routes/user.js
@@ -5,4 +5,5 @@ const router = express.Router();
 router.post("/signup", UserController.createUser);
 router.post("/login", UserController.userLogin);
 
+
 module.exports = router;
